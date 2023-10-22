5ca498f36f3627c78a04970fdbb527beebd83a04
CAO TRẦN ANH KHOA <108146451+caotrananhkhoa@users.noreply.github.com>
2023-10-22 18:46:21 +0700

    Update page.tsx

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
     }
 
     return (
-        <section className="px-4 py-10 sm:px-6 sm:py-16 lg:px8 lg:py-24">
+        <section className="px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
             <Login />
         </section>
     )
